e76f249e6853b6f91d71ce10d8ec05a12e58c98d
Chia Yong Kang <chiayongkang@hotmail.com>
2024-03-30 16:32:45 +0800

    removed comments

--- optimizer/calldata.go
+++ optimizer/calldata.go
@@ -17,22 +17,17 @@ var validTypes = map[string]bool{
 func (o *Optimizer) optimizeCallData() {
 	contracts := o.builder.GetRoot().GetContracts()
 	for _, contract := range contracts {
-		// iterate through the contract's functions
 		functions := contract.GetFunctions()
 		for _, f := range functions {
 			if f.GetVisibility() == ast_pb.Visibility_EXTERNAL {
-				// update the function to use call data instead of memory
-				// f.SetStateMutability(ast_pb.Mutability_VIEW)
 				parameters := f.GetParameters()
 
 				for _, param := range parameters {
-					// Get the type of the parameter
 					paramType := param.Unit.GetTypeName().GetName()
 					// Check if the type is valid for storage
 					if _, ok := validTypes[paramType]; !ok {
 						continue
 					}
-					// check if the parameter storage is memory
 					if param.Unit.StorageLocation == ast_pb.StorageLocation_MEMORY {
 						param.Unit.StorageLocation = ast_pb.StorageLocation_CALLDATA
 					}
