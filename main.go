34c231b375630f600de527194fc29334214d7bba
Evan Chng Xian Lin <xianlinchng@gmail.com>
2024-04-09 18:18:43 +0800

    clean up main.go

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"optimizer/optimizer/logger"
 	"optimizer/optimizer/optimizer"
 	"optimizer/optimizer/printer"
 	"os"
+	"path/filepath"
 
 	"go.uber.org/zap"
 )
@@ -22,10 +24,10 @@ func main() {
 		zap.L().Error("Failed to get current working directory", zap.Error(err))
 	}
 
+	config := GetConfig()
+
 	// join the current working directory with the file path
-	// filepath := cwd + "/examples/unoptimized_contracts/struct_packing.sol"
-	// filepath := cwd + "/examples/unoptimized_contracts/calldata.sol"
-	filepath := cwd + "/examples/unoptimized_contracts/storage_variable_caching.sol"
+	filepath := filepath.Join(cwd, config.filepath)
 
 	builder, err := printer.GetBuilder(ctx, filepath)
 	if err != nil {
@@ -46,10 +48,68 @@ func main() {
 		zap.L().Error("Failed to resolve references", zap.Errors("resolve errors", errs))
 	}
 
-	// rootNode := ast.GetRoot() // for printing
+	if config.printOutput {
+		fmt.Println("UNOPTIMIZED====================")
+		fmt.Println(ast.GetRoot().ToSource())
+		fmt.Println("================================")
+	}
 	opt := optimizer.NewOptimizer(builder)
-	// opt.PackStructs()
-	// opt.OptimizeCallData()
-	opt.CacheStorageVariables()
-	fmt.Println(builder.GetRoot().Unit.ToSource())
+	if config.packStructs {
+		opt.PackStructs()
+	}
+	if config.optimizeCallData {
+		opt.OptimizeCallData()
+	}
+	if config.cacheStorageVariables {
+		opt.CacheStorageVariables()
+	}
+
+	if config.printOutput {
+		fmt.Println("OPTIMIZED======================")
+		fmt.Println(ast.GetRoot().ToSource())
+		fmt.Println("================================")
+	}
+}
+
+type Config struct {
+	filepath              string
+	packStructs           bool
+	optimizeCallData      bool
+	cacheStorageVariables bool
+	printOutput           bool
+}
+
+func GetConfig() Config {
+	// use the flag library to parse the command line arguments
+	var (
+		filepath              string
+		packStructs           bool
+		optimizeCallData      bool
+		cacheStorageVariables bool
+		printOutput           bool
+	)
+	flag.StringVar(&filepath, "file", "", "The path to the file to optimize")
+	flag.BoolVar(&packStructs, "pack-structs", false, "Pack structs")
+	flag.BoolVar(&optimizeCallData, "optimize-call-data", false, "Optimize call data")
+	flag.BoolVar(&cacheStorageVariables, "cache-storage-variables", false, "Cache storage variables")
+	flag.BoolVar(&printOutput, "print-output", false, "Print the output")
+	flag.Parse()
+
+	fmt.Println("Starting with the following configuration:")
+	fmt.Println("  filepath:", filepath)
+	fmt.Println("  pack-structs:", packStructs)
+	fmt.Println("  optimize-call-data:", optimizeCallData)
+	fmt.Println("  cache-storage-variables:", cacheStorageVariables)
+	fmt.Println("  print-output:", printOutput)
+
+	if filepath == "" {
+		zap.L().Fatal("File path is required")
+	}
+	return Config{
+		filepath:              filepath,
+		packStructs:           packStructs,
+		optimizeCallData:      optimizeCallData,
+		cacheStorageVariables: cacheStorageVariables,
+		printOutput:           printOutput,
+	}
 }
