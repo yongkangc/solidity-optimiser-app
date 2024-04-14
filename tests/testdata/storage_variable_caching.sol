546564de8961d4ecf92b276b5c0d4cba23e02139
Evan Chng Xian Lin <xianlinchng@gmail.com>
2024-04-14 13:59:08 +0800

    fix storage var test case

--- tests/testdata/storage_variable_caching.sol
+++ tests/testdata/storage_variable_caching.sol
@@ -5,13 +5,14 @@ contract Counter1 {
     uint256 public number;
     int256[] public arr;
 
-    function increment() public view {
+    function increment() public view returns (uint256) {
         require(number < 10);
-        number = number + 1;
+        uint256 incremented = number + 1;
+        return incremented;
     }
 
     // probably shouldnt optimise this
-    function sum() public returns (int256) {
+    function sum() public view returns (int256) {
         int256 sum = 0;
         for (uint256 i = 0; i < arr.length; i++) {
             sum += arr[i];
